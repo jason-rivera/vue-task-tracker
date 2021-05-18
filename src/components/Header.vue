0e4d27ae558d8ff63eac2f9933f608a53a44a27c
Jason Rivera <jp_rivera_@hotmail.com>
2021-05-18 14:20:52 -0700

    updated

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button';
+export default {
+  name: 'Header',
+  /*1.*/
+  // props: ['title'] 
+  /*2.*/
+  // props: {
+  //   title: String
+  // }
+  /*3.*/
+  props: {
+    title: {
+      type: String,
+      default: "Hello World"
+    }
+  },
+
+  components: {
+    Button
+  }
+};
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
